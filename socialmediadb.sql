13d5c157c1b1113190203f362de33cfd92d63ef9
Michelle Fronda <148023323+frawnda@users.noreply.github.com>
2026-04-12 14:09:49 -0400

    Create MESSAGE, NOTIFICATION, and GROUP tables
    Added MESSAGE, NOTIFICATION, and GROUP tables with relationships.

--- socialmediadb.sql
+++ socialmediadb.sql
@@ -8,4 +8,43 @@ USE socialmedia;
 -- Create the USER table
 CREATE TABLE user (
    userID INT PRIMARY KEY,
-)
+   
+);
+
+
+-- Create the MESSAGE table
+CREATE TABLE Message (
+    MessageID INT PRIMARY KEY,
+    SenderID INT,
+    ReceiverID INT,
+    Content TEXT,
+    Timestamp DATETIME,
+    read_status VARCHAR(10),
+    FOREIGN KEY (SenderID) REFERENCES User(UserID),
+    FOREIGN KEY (ReceiverID) REFERENCES User(UserID)
+);
+
+-- Create the NOTIFICATION table
+CREATE TABLE Notification (
+    NotificationID INT PRIMARY KEY,
+    UserID INT,
+    RelatedPostID INT,
+    RelatedMessageID INT,
+    notification_type VARCHAR(20),
+    Content TEXT,
+    Timestamp DATETIME,
+    read_status VARCHAR(10),
+    FOREIGN KEY (UserID) REFERENCES User(UserID),
+    FOREIGN KEY (RelatedPostID) REFERENCES Post(PostID),
+    FOREIGN KEY (RelatedMessageID) REFERENCES Message(MessageID)
+);
+
+-- Create the GROUP table
+CREATE TABLE Group (
+    GroupID INT PRIMARY KEY,
+    CreatorID INT,
+    group_name VARCHAR(20),
+    privacy_setting VARCHAR(10),
+    date_created DATETIME,
+    FOREIGN KEY (CreatorID) REFERENCES User(UserID)
+);
